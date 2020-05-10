0df8043df56c846c49e5bae2a70b0fea04a6aee8
soo-underground <bordovskiy92@gmail.com>
2020-05-10 17:56:19 +0300

    pep8ified code

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from nltk.stem import WordNetLemmatizer
 import flask
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -20,47 +21,56 @@ CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
 with open('YaTranslateKey.key', 'r') as file:
-    auth =file.read().replace('\n', '')
+    auth = file.read().replace('\n', '')
+
 
 def load_classifier():
     classifier = pickle.load(open('models/multilabel_model.sav', 'rb'))
     return classifier
 
+
 def load_tfidf():
     tfidf = pickle.load(open('models/tfidf.sav', 'rb'))
     return tfidf
+
 
 def load_binarizer():
     binarizer = pickle.load(open('models/multilabel_binarizer.sav', 'rb'))
     return binarizer
 
+
 def clean_text(text):
     text = re.sub("\'", "", text)
-    text = re.sub("[^a-zA-Z]"," ",text)
+    text = re.sub("[^a-zA-Z]", " ", text)
     text = ' '.join(text.split())
     text = text.lower()
     return text
 
-from nltk.stem import WordNetLemmatizer
+
 wordnet_lemmatizer = WordNetLemmatizer()
+
+
 def lemmatize_text(text):
-    lemmatized_text=[]
+    lemmatized_text = []
     sentence_words = nltk.word_tokenize(text)
     for word in sentence_words:
         lemmatized_text.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
     lemmatized_text = ' '.join(lemmatized_text)
     return lemmatized_text
 
+
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
+
+
 def remove_stopwords(text):
     no_stopword_text = [w for w in text.split() if not w in stop_words]
     return ' '.join(no_stopword_text)
 
-@app.route('/predict', methods=['POST'])
 
+@app.route('/predict', methods=['POST'])
 def predict():
     global auth
     request_json = request.get_json()
@@ -84,14 +94,14 @@ def predict():
     q_vec = tfidf.transform([x])
     q_pred = classifier.predict_proba(q_vec)
 
-    suggested_labels=[]
+    suggested_labels = []
     counter = 0
     for x in binarizer.classes_:
-        proba  = round(q_pred[0][counter],2)
+        proba = round(q_pred[0][counter], 2)
         if proba > 0.75:
             suggested_labels.append(str(x))
-        counter +=1
-    prediction=suggested_labels
+        counter += 1
+    prediction = suggested_labels
 
     prediction = str(prediction).strip('[()]').replace("'", "")
 
@@ -100,6 +110,7 @@ def predict():
     prediction = prediction.split(", ")
     response = json.dumps({'response': prediction}, ensure_ascii=False)
     return response, 200
+
 
 if __name__ == '__main__':
     application.run(debug=True)
