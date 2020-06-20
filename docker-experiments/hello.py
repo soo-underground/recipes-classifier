02b9cbd91a86c07db14ad3e8a6fb78d4d1180c27
soo-underground <bordovskiy92@gmail.com>
2020-06-20 17:56:21 +0300

    added params to test routes

--- docker-experiments/hello.py
+++ docker-experiments/hello.py
@@ -4,4 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print('log: new request!!!')
-    return 'Hello, world!'
+    return 'Hello, world!!!'
+
+@app.route('/name/<myname>')
+def hello_name(myname):
+    print('log: new request from %s!!!' % myname)
+    return 'Hello, %s!!!' % myname
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+@app.route('/avg/<nums>/')
+def avg(nums):
+    nums = nums.split(',')
+    nums = [float(num) for num in nums]
+    nums_mean = mean(nums)
+    return 'average is %s!!!' % str(nums_mean)
