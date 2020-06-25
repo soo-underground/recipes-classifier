8e789e08263dca62593d6321c1b6e2d2e4d39de2
soo-underground <bordovskiy92@gmail.com>
2020-06-25 10:52:53 +0300

    test ml in docker

--- docker-experiments/hello.py
+++ docker-experiments/hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask.logging import create_logger
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -23,3 +26,35 @@ def avg(nums):
     nums = [float(num) for num in nums]
     nums_mean = mean(nums)
     return 'average is %s!!!' % str(nums_mean)
+
+@app.route('/iris/<param>')
+def iris(param):
+
+    param = param.split(',')
+    param = [float(param) for param in param]
+
+    LOG.info(param)
+
+    iris = datasets.load_iris()
+    iris_X = iris.data 
+    iris_y = iris.target 
+
+    np.random.seed(0)
+    indices = np.random.permutation(len(iris_X))
+    iris_X_train = iris_X[indices[:-10]] 
+    iris_y_train = iris_y[indices[:-10]] 
+    iris_X_test = iris_X[indices[-10:]] 
+    iris_y_test = iris_y[indices[-10:]]
+
+    knn = KNeighborsClassifier()
+    knn.fit(iris_X_train, iris_y_train) 
+    KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+                         metric_params=None, n_jobs=1, n_neighbors=5, p=2,
+                         weights='uniform'
+                         )
+    knn.predict(iris_X_test)
+
+    param = np.array(param).reshape(1,-1)
+    predict = knn.predict(param)
+
+    return str(predict)
